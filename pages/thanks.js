981ebe2a6d9a9d0122033c9eb0895b30f863c731
sarah faust <sarah.faustmann@gmail.com>
2021-10-18 12:30:25 +0200

    Add thank you page

---
+++ pages/thanks.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import { ColCard, Container, h2Style } from '../styles';
+
+export default function Thanks(props) {
+  return (
+    <Container>
+      <ColCard>
+        <h2 css={h2Style}>Thank you!</h2>
+        <div>
+          Thank you for your purchase. We will ship it within the next two
+          business days. Check your email or account get updates on your order.
+          Reach out with any questions to sarah@boardgameshop.at.
+        </div>
+      </ColCard>
+    </Container>
+  );
+}
