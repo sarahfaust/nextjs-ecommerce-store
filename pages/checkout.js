d3ae96727f4ddb0522eab39fae7621ec1a73403b
sarah faust <sarah.faustmann@gmail.com>
2021-10-18 15:14:04 +0200

    Add functionality to checkout page

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { useRouter } from 'next/dist/client/router';
 import { useState } from 'react';
 import { Button } from '../components/Button';
 import { ColCard, Container, h2Style } from '../styles';
@@ -30,22 +31,50 @@ export default function Checkout(props) {
   const [city, setCity] = useState('');
   const [postcode, setPostcode] = useState('');
 
+  const router = useRouter();
+
   return (
     <Container>
       <ColCard>
         <h2 css={h2Style}>Checkout</h2>
         <Form>
           <Label htmlFor="firstname">First name</Label>
-          <Input id="firstname" name="firstname" />
+          <Input
+            id="firstname"
+            name="firstname"
+            value={firstName}
+            onChange={(event) => setFirstName(event.currentTarget.value)}
+          />
           <Label htmlFor="lastname">Last name</Label>
-          <Input id="lastname" name="lastname" />
+          <Input
+            id="lastname"
+            name="lastname"
+            value={lastName}
+            onChange={(event) => setLastName(event.currentTarget.value)}
+          />
           <Label htmlFor="address">Address</Label>
-          <Input id="address" name="address" />
+          <Input
+            id="address"
+            name="address"
+            value={address}
+            onChange={(event) => setAddress(event.currentTarget.value)}
+          />
           <Label htmlFor="city">City</Label>
-          <Input id="city" name="city" />
-          <Label htmlFor="postcode">Postcode</Label>
+          <Input
+            id="city"
+            name="city"
+            value={city}
+            onChange={(event) => setCity(event.currentTarget.value)}
+          />
+          <Label
+            htmlFor="postcode"
+            value={postcode}
+            onChange={(event) => setPostcode(event.currentTarget.value)}
+          >
+            Postcode
+          </Label>
           <Input id="postcode" name="postcode" />
-          <Button>Submit</Button>
+          <Button onClick={() => router.push('/thanks')}>Submit</Button>
         </Form>
       </ColCard>
     </Container>
